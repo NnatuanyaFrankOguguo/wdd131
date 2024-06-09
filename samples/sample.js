b06f9500d3edb68ec27ded17166e8098bb620420
Nnatuanya Frank <nnatuanyafrank@gmail.com>
2024-06-09 23:19:47 +0100

    recent

---
+++ samples/sample.js
@@ -0,0 +1,7 @@
+let myString = 'developer';
+
+
+myNewString = myString.charAt(0).toUpperCase() + myString.slice(1);
+
+
+console.log(myNewString);
--- sandbox/debugging.js
+++ sandbox/debugging.js
@@ -1,6 +1,31 @@
-const PI = 3.14;
-const radius = 3;
-let area = 0;
-area = radius * radius * pi;
-radius = 4;
-area = radius * radius * pi;
+const aCourse = {
+  code: 'CSE121b',
+  name: 'Javascript Language',
+  section: [
+    {
+      sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'
+    },
+    {
+      sectionNum: 2,
+      roomNum: 'STC 347',
+      enrolled: 28,
+      days: 'TTh',
+      instructor: 'Sis A'
+    },
+  ],
+};
+
+function addcourse(course){
+  const courseName = document.querySelector('#courseName');
+  const courseCode = document.querySelector('#courseCode');
+  courseName.textContent = aCourse.name;
+  courseCode.textContent = aCourse.code;
+}  
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// Calling the function
+console.log(greet('Alice')); // Output: Hello, Alice!
+
+  
