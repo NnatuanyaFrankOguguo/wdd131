c2f4f13cfa33dd7e3ae11f1262f4854c33530819
Nnatuanya Frank <nnatuanyafrank@gmail.com>
2024-06-29 13:06:35 +0100

    ok

--- scripts/place.js
+++ scripts/place.js
@@ -17,3 +17,31 @@ lastModified.textContent = 'Last Modification: ' + formattedDate + ' ' + formatt
 lastModified.style.fontSize = '0.9em'
 lastModified.style.paddingRight = '20px'
 
+
+let temperature = 10;
+let windspeed = 4.9;
+
+
+function calculateWindChill(temperature, windspeed){
+    const parentElement = document.querySelector('#windchill-parent');
+    const newP = document.createElement('p');
+    if (temperature <= 10 && windspeed >= 4.8 ){
+        let windChill = 13.12 + 0.6215 * temperature - 11.37 * windspeed ** 0.16 + 0.3965 * temperature * windspeed ** 0.16;
+        newP.innerHTML = `<span>Windchill:</span> ${windChill.toFixed(2)}`
+        newP.style.padding = '3px'  
+        parentElement.appendChild(newP)
+        return windChill;
+        
+    } else {
+        newP.innerHTML = `<span>Windchill:</span> N/A `
+        parentElement.appendChild(newP)
+        return 'N/A';
+        
+
+    }
+
+}
+
+
+calculateWindChill(temperature, windspeed)
+
