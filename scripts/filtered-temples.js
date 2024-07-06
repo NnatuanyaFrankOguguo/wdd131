60751b19aebd7adc931e40cea31f91b6dcbeb416
Nnatuanya Frank <nnatuanyafrank@gmail.com>
2024-07-06 09:19:27 +0100

    ok

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -202,7 +202,7 @@ function createTempleCard(filteredTemples){
 
      
 
-    name.textContent = temple.templeName;
+    name.innerHTML =  `<h2> ${temple.templeName} </h2>`;
     name.style.marginBottom = '15px';
     location.innerHTML = `<span class="label"> Location: </span> ${temple.location}`;
     dedicated.innerHTML = `<span class="label"> Dedicated: </span> ${temple.dedicated} `;
